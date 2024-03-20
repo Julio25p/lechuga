7d30f556ac2780f0c1a223dfe019e3fc736cd454
Julio25p <163605103+Julio25p@users.noreply.github.com>
2024-03-19 19:32:32 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -11,13 +11,17 @@ function extractData() {
         'CREATININA SERICA': 'CREA',
         'UREA': 'UREA',
         'GLUCOSA': 'GLU',
+        'HbA1c en unidades NGSP': 'HbA1c',
         'ACTIVADO': 'TPT',
         'TIEMPO DE PROTROMBINA': 'TP',
         'TRIGLICERIDOS': 'TAG',
         'COLESTEROL': 'COL',
-        'SODIO': 'Na',
-        'POTASIO': 'K',
-        'CLORO': 'Cl',
+        'ACIDO URICO': 'AU',
+        'Sodio': 'Na',
+        'Potasio': 'K',
+        'Cloro': 'Cl',
+        'CALCIO SERICO': 'Ca',
+        'FOSFORO SERICO': 'Po',
         'DESHIDROGENASA LÁCTICA (L)': 'LDH',
         'FOSFATASA ALCALINA': 'FA',
         'ALBUMINA': 'ALB',
@@ -25,12 +29,11 @@ function extractData() {
         'ALANINA DE AMINOTRANSFERASA (ALTTGP)': 'ALT',
         'BILIRRUBINA DIRECTA': 'BD',
         'BILIRRUBINA INDIRECTA': 'BI',
-        'PH': 'pH',
-        'PCO2': 'PCo2',
-        'PO2': 'Po2',
-        'LACTATO': 'LAC',
-        'HC03 ACTUAL': 'HC03',
-        '% DE SATURACIÓN O2': 'SATO2',
+        'BILIRRUBINAS TOTALES': 'BT',
+        'pH': 'pH',
+        'PCO2': 'PCO2',
+        'PO2': 'PO2',
+        'HC03 actual': 'HC03',
         'Proteinas': 'PROT',
         'Glucosa': 'GLUC',
         'Cuerpos cetónicos': 'CETONAS',
@@ -40,24 +43,40 @@ function extractData() {
         'Bacterias': 'BAC',
         'Levaduras': 'LEV',
         'Cilindros': 'CILINDROS',
-        'EXAMEN GENERAL DE ORINA': 'EGO'
+        'EXAMEN GENERAL DE ORINA': 'EGO',
+        'TROPONIN I': 'Trop I',
+        'CREATINFOSFOQUINASA (CPK TOTAL)': 'CPK',
+        'CK-MB': 'CK-MB',
+        'FT4 TIROXINA LIBRE': 'T4',
+        'FT3 TRIYODOTIRONINA LIBRE':'T3',
+        'HORMONA ESTIMULANTE TIROIDES': 'TSH',
+        'AMILASA SERICA': 'AMILASA',
+        'PROTEINA C REACTIVA (hs-CRP)': 'PCR'
     };
-    var outputText = '';
+    var outputData = {};
     for (var i = 0; i < lines.length; i++) {
         var line = lines[i];
         for (var lab in labData) {
-            if (line.includes(lab)) {
-                if (lab === 'EXAMEN GENERAL DE ORINA') {
-                    outputText += labData[lab] + ' ';
+            if (line.includes(lab) && !(labData[lab] in outputData)) {
+                var value;
+                if (lab === 'EXAMEN GENERAL DE ORINA' || lab === 'ACTIVADO') {
+                    value = lines[i + 1].trim().split(' ')[0];
                 } else {
-                    var value = line.split(lab)[1].trim().split(' ')[0];
-                    outputText += labData[lab] + ': ' + value + ', ';
+                    value = line.split(lab)[1].trim().split(' ')[0];
+                    if (value == "") {
+                        value = lines[i + 1].trim().split(' ')[0];
+                    }
                 }
+                outputData[labData[lab]] = value;
             }
         }
     }
+    var outputText = '';
+    for (var lab in outputData) {
+        outputText += lab + ': ' + outputData[lab] + ', ';
+    }
     var date = new Date();
-    var formattedDate = date.getDate() + '/' + (date.getMonth() + 1);
+    var formattedDate = date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
     outputText = formattedDate + ' ' + outputText;
     document.getElementById('outputArea').innerText = outputText;
 }
@@ -66,4 +85,5 @@ function copyToClipboard() {
     var outputText = document.getElementById('outputArea').innerText;
     navigator.clipboard.writeText(outputText);
 }
+
 
