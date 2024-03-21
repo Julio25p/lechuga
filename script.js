f43491d68bb8e12c1789daec597d8825e0d97d36
Julio25p <163605103+Julio25p@users.noreply.github.com>
2024-03-20 21:46:16 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,38 +2,50 @@ function extractData() {
     var inputText = document.getElementById('inputArea').value;
     var lines = inputText.split('\n');
     var labData = {
+        //Bh
         'ERITROCITOS': 'ERI',
         'HEMOGLOBINA': 'HB',
         'HEMATOCRITO': 'HTO',
         'LEUCOCITOS': 'LEU',
         'NEUTROFILOS SEGMENTADOS %': 'NEU SEG',
         'PLAQUETAS': 'PLAQ',
+        //QS
         'CREATININA SERICA': 'CREA',
         'UREA': 'UREA',
         'GLUCOSA': 'GLU',
         'HbA1c en unidades NGSP': 'HbA1c',
-        'ACTIVADO': 'TPT',
-        'TIEMPO DE PROTROMBINA': 'TP',
         'TRIGLICERIDOS': 'TAG',
         'COLESTEROL': 'COL',
         'ACIDO URICO': 'AU',
+        //TIEMPOS
+        'ACTIVADO': 'TPT',
+        'TIEMPO DE PROTROMBINA': 'TP',
+        'I.N.R.': 'INR',
+        //Eles
         'Sodio': 'Na',
         'Potasio': 'K',
         'Cloro': 'Cl',
         'CALCIO SERICO': 'Ca',
         'FOSFORO SERICO': 'Po',
+        'MAGNESIO SERICO': 'MG',
+        //PFH
         'DESHIDROGENASA LÁCTICA (L)': 'LDH',
         'FOSFATASA ALCALINA': 'FA',
         'ALBUMINA': 'ALB',
+        'GLOBULINAS': 'GLOB',
         'ASPARTATO DE AMINOTRANSFERASA (TGO)': 'AST',
         'ALANINA DE AMINOTRANSFERASA (ALTTGP)': 'ALT',
         'BILIRRUBINA DIRECTA': 'BD',
         'BILIRRUBINA INDIRECTA': 'BI',
         'BILIRRUBINAS TOTALES': 'BT',
+        //Gaso
         'pH': 'pH',
         'PCO2': 'PCO2',
-        'PO2': 'PO2',
+        'Lactato': 'Lac',
         'HC03 actual': 'HC03',
+        '(EBA)': 'EBA',
+        '(SAT)': 'SATO2',
+        //'EXAMEN GENERAL DE ORINA': 'EGO',
         'Proteinas': 'PROT',
         'Glucosa': 'GLUC',
         'Cuerpos cetónicos': 'CETONAS',
@@ -42,7 +54,7 @@ function extractData() {
         'Leucocitos': 'LEUC',
         'Bacterias': 'BAC',
         'Levaduras': 'LEV',
-        //'EXAMEN GENERAL DE ORINA': 'EGO',
+        //ENZIMAS
         'TROPONIN I': 'Trop I',
         'CREATINFOSFOQUINASA (CPK TOTAL)': 'CPK',
         'CK-MB': 'CK-MB',
@@ -50,8 +62,16 @@ function extractData() {
         'FT3 TRIYODOTIRONINA LIBRE':'T3',
         'HORMONA ESTIMULANTE TIROIDES': 'TSH',
         'AMILASA SERICA': 'AMILASA',
+        'LIPASA SERICA': 'LIPASA',
         'PROTEINA C REACTIVA (hs-CRP)': 'PCR',
-        'ANTIGENO PROSTATICO ESPECIFICO (PSA)': 'PSA'
+        'ANTIGENO PROSTATICO ESPECIFICO (PSA)': 'PSA',
+        //MARCADORES
+        'ALFA 1-FETOPROTEINA':'A-FETOPROT',
+        'ANTIGENO CA 19-9': 'CA19-9',
+        'ANTIGENO CARCINO EMBRIONARIO': 'A-CARCINOEMB',
+        'ANTIGENO CA 15-3': 'CA13-3',
+        'VELOCIDAD DE SEDIMENTACION GLOBULAR': 'VSG',
+        'PROLACTINA': 'PRL'
     };
     var outputData = {};
     for (var i = 0; i < lines.length; i++) {
